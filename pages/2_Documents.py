3f0d8d7bdf524360051fd3310a28df1e3e05c0b8
Jana Aljouni <108636622+JanaAljouni@users.noreply.github.com>
2024-12-29 16:36:42 +0300

    Add files via upload

---
+++ pages/2_Documents.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import base64
+from constant import *
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("style/style.css")
+
+
+
+st.title("💻 Documents")
+st.write('Below you can find all the downloadable documents')
+
+
+with open("\\portfolio-template\\images\\resume.pdf", "rb") as pdf_file:
+    PDFbyte = pdf_file.read()
+
+with open("\\portfolio-template\\images\\GP1.pdf", "rb") as pdf_file:
+    PDFbyte1 = pdf_file.read()
+
+with open("\\portfolio-template\\images\\Transcript .pdf", "rb") as pdf_file:
+    PDFbyte2 = pdf_file.read()
+
+with open("\\portfolio-template\\images\\Degree_of_honor.pdf", "rb") as pdf_file:
+    PDFbyte3 = pdf_file.read()
+
+
+
+st.download_button(
+    label="Resume",
+    data=   PDFbyte ,
+    file_name="resume.pdf",
+    mime="application/octet-stream",
+)
+
+
+st.download_button(
+    label="Quantum Analyzer doc",
+    data=PDFbyte1,
+    file_name="Quantum.pdf",
+    mime="application/octet-stream",
+)
+
+st.download_button(
+    label="Transcript",
+    data=PDFbyte2,
+    file_name="Transcript.pdf",
+    mime="application/octet-stream",
+)
+
+st.download_button(
+    label="Degree of Honor",
+    data=PDFbyte3,
+    file_name="degree_of_honor",
+    mime="application/octet-stream",
+)
+
---
+++ pages/3_Contact_me.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import base64
+from constant import *
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        
+local_css("style/style.css")
+
+
+
+st.subheader("📨 Contact Me")
+contact_form = f"""
+        <form action="https://formsubmit.co/{info["Email"]}" method="POST">
+            <input type="hidden" name="_captcha value="false">
+            <input type="text" name="name" placeholder="Your name" required>
+            <input type="email" name="email" placeholder="Your email" required>
+            <textarea name="message" placeholder="Your message here" required></textarea>
+            <button type="submit">Send</button>
+        </form>
+        """
+st.markdown(contact_form, unsafe_allow_html=True)
